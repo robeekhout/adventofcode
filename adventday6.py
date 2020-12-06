69d9e13c8cd20ef96c85fb110085124c9f35cfff
rob <robeekhout@gmail.com>
2020-12-06 12:51:36 +0100

    day 6 finished both parts

--- adventday6.py
+++ adventday6.py
@@ -1,4 +1,5 @@
 from typing import List
+from collections import Counter
 
 
 def read_input(textfile: str) -> List:
@@ -23,11 +24,22 @@ def count_yes_per_group(group: List) -> int:
     return len(all_questions_with_yes)
 
 
+def count_consensus_yes_per_group(group: List) -> int:
+    result = 0
+    counted = Counter()
+    for person in group:
+        counted += Counter(person)
+    for key in counted:
+        if counted[key] == len(group):
+            result += 1
+    return result
+
+
 def main():
     part_1 = sum([count_yes_per_group(x) for x in read_input("adventday6input")])
     print(f"The answer to part 1 is: {part_1}")
-
-    # print(f"The answer to part 2 is: {part_2}")
+    part_2 = sum([count_consensus_yes_per_group(x) for x in read_input("adventday6input")])
+    print(f"The answer to part 2 is: {part_2}")
 
 
 if __name__ == "__main__":
