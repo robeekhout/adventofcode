1d28e924cac0d4d49b497a80b1087f9441ee4eb4
rob <robeekhout@gmail.com>
2020-12-19 22:00:42 +0100

    finished day 9

--- days/adventday9.py
+++ days/adventday9.py
@@ -1,5 +1,6 @@
-from typing import List
 from itertools import combinations
+
+from typing import List
 
 
 def parse_input(textfile: str) -> List[int]:
@@ -8,15 +9,29 @@ def parse_input(textfile: str) -> List[int]:
     return data
 
 
-def find_incorrect_number(xmas_data):
+def find_incorrect_number(xmas_data: list) -> int:
     for ix in range(25, len(xmas_data)):
         if xmas_data[ix] not in list(map(sum, combinations(xmas_data[ix - 25:ix], 2))):
             return xmas_data[ix]
 
 
+def find_encryption_weakness(xmas_data: List[int], invalid_number: int) -> int:
+    for ix in range(len(xmas_data)):
+        running_sum = 0
+        running_ix = ix
+        while running_sum <= invalid_number:
+            if running_sum == invalid_number:
+                return min(xmas_data[ix:running_ix + 1]) + max(xmas_data[ix:running_ix + 1])
+            else:
+                running_sum += xmas_data[running_ix]
+                running_ix += 1
+
+
 def main():
     part_1 = find_incorrect_number(parse_input("../inputs/adventday9input"))
     print(f"The answer to part 1 is: {part_1}")
+    part_2 = find_encryption_weakness(parse_input("../inputs/adventday9input"), part_1)
+    print(f"The answer to part 2 is: {part_2}")
 
 
 if __name__ == "__main__":
